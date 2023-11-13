ed081cae8ae2f8d72c2826d0fbb8126045db925a
sujitbaram <sujitbaram@gmail.com>
2023-11-13 18:11:29 +0530

    devops_class

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"  # Specify your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Specify the AMI ID for your desired operating system
+  instance_type = "t2.micro"                 # Specify the instance type
+
+  user_data = <<-EOF
+              #!/bin/bash
+               sudo apt update -y
+               sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+               sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+               sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+               sudo apt update -y
+               sudo apt install docker-ce -y
+               sudo apt install nginx -y
+               EOF
+
+  tags = {
+    Name = "Devops-Class"  # Specify the desired name for your instance
+  }
+}
